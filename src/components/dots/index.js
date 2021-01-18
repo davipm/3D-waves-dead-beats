b172b4797b7d48df2f6b25cf027fe803e93b1ae5
davi <davi.p.m94@gmail.com>
2021-01-18 19:31:40 -0300

    adding dots

---
+++ src/components/dots/index.js
@@ -0,0 +1,56 @@
+import { useRef, useMemo } from "react";
+import { useFrame } from "react-three-fiber";
+import * as THREE from "three";
+
+import { roundedSquareWave } from "../../utils";
+
+export default function Dots() {
+  const ref = useRef();
+  const { vec, transform, position, distance } = useMemo(() => {
+    const vec = new THREE.Vector3();
+    const transform = new THREE.Matrix4();
+
+    const position = [...Array(10000)].map((_, i) => {
+      const position = new THREE.Vector3();
+
+      position.x = (i % 100) - 50;
+      position.y = Math.floor(i / 100) - 50;
+
+      position.y += (i % 2) * 0.5;
+
+      position.x += Math.random() * 0.3;
+      position.y += Math.random() * 0.3;
+
+      return position;
+    });
+
+    const right = new THREE.Vector3(1, 0, 0);
+
+    const distance = position.map((pos) => {
+      return pos.length() + Math.cos(pos.angleTo(right) * 8) * 0.5;
+    });
+
+    return { vec, transform, position, distance };
+  }, []);
+
+  useFrame(({ clock }) => {
+    for (let i = 0; i < 10000; i++) {
+      const dist = distance[i];
+      const t = clock.elapsedTime - dist / 25;
+      const wave = roundedSquareWave(t, 0.15 + (0.2 * dist) / 72, 0.4, 1 / 3.8);
+
+      vec.copy(position[i]).multiplyScalar(wave + 1.3);
+      transform.setPosition(vec);
+      ref.current.setMatrixAt(i, transform);
+    }
+
+    ref.current.instanceMatrix.needsUpdate = true;
+  });
+
+  return (
+    <instancedMesh ref={ref} args={[null, null, 10000]}>
+      <circleBufferGeometry args={[0.15]} />
+      <meshBasicMaterial />
+    </instancedMesh>
+  );
+}
