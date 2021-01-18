43b070e85bb3c3170cab68e1c9ac462d130b04b7
davi <davi.p.m94@gmail.com>
2021-01-18 19:32:14 -0300

    adding loading component

---
+++ src/components/loading/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Loading = () => (
+  <div className="loading-content">
+    <div id="load">
+      <div>G</div>
+      <div>N</div>
+      <div>I</div>
+      <div>D</div>
+      <div>A</div>
+      <div>O</div>
+      <div>L</div>
+    </div>
+  </div>
+);
+
+export default Loading;
